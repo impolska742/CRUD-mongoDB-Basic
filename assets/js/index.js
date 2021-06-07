6f7a00f2b52d804e4d6ca417d031d9d6b80d6b40
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2021-06-07 20:16:30 +0530

    URL Changed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+const appUrl = "https://calm-headland-70598.herokuapp.com/";
+
 $("#add_user").submit(function (event) {
   alert("Data inserted successfully");
 });
@@ -15,7 +17,7 @@ $("#update_user").submit(function (event) {
   // console.log(data);
 
   var request = {
-    url: `http://localhost:3000/api/users/${data.id}`,
+    url: appUrl + `/api/users/${data.id}`,
     method: "PUT",
     data: data,
   };
